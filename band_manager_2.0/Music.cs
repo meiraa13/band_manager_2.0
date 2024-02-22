44c6baec0732b7df3e136a5f02c39de5cde567ac
Marcelo Meirelles <meirellesm@hotmail.com>
2024-02-21 21:09:30 -0300

    feat: add class Music and its properties

---
+++ band_manager_2.0/Music.cs
@@ -0,0 +1,26 @@
+﻿class Music
+{
+    public string Name {  get; set; }
+    public string Artist {  get; set; }
+    public int Duration {  get; set; }
+    public bool Available { get; set; }
+    // lambda ou "arrow function" servindo como getter
+    public string BandDescription => $"A música {Name} pertence à banda {Artist}";
+
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Nome: {this.Name}");
+        Console.WriteLine($"Artista: {this.Artist}");
+        Console.WriteLine($"Duração: {this.Duration}");
+        if (Available)
+        {
+            Console.WriteLine("Disponível no plano");
+        }else
+        {
+            Console.WriteLine("Adquira o plano plus");
+        }
+
+    }
+
+}
--- band_manager_2.0/Program.cs
+++ band_manager_2.0/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Music music1 = new Music();
+music1.Name = "Roxanne";
+music1.Artist = "The Police";
+
+music1.ShowInfo();
+
+Music music2 = new Music();
+music2.Name = "Vertigo";
+music2.Artist = "U2";
